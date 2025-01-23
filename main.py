7921018c6be5eb598d6cab193e0a5f35aadab603
João Vicente Nicola Feltrin <joaodarklegal@gmail.com>
2025-01-23 22:09:03 +0000

    Criar uma função para calcular a expressão

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ def click_button(value):
     
     entry.delete(0, tk.END)
     entry.insert(tk.END, current + value)
+def calcular():
+    try:
+       
+        result = eval(entry.get())  
+        entry.delete(0, tk.END)  
+        entry.insert(tk.END, result)  
+    except Exception as e:
+        
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, "Erro")
